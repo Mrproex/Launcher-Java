1b106fe6c5a9bd4052b81751cf489e20ec6933f2
Julien Moes <julien.moes@gmail.com>
2022-04-10 03:10:22 +0200

    Firt results with javax.swing.

--- src/proex/launcher/main/Components.java
+++ src/proex/launcher/main/Components.java
@@ -1,24 +1,46 @@
 package proex.launcher.main;
 
 import java.awt.Color;
+import java.awt.Event;
 import java.awt.Image;
 import javax.swing.*;
 
+import proex.launcher.main.Extensions.JButtonWithBackground;
 import proex.launcher.main.Extensions.JPanelWithBackground;
 
 public class Components 
 {
 	public Components() {
 		//frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 		frame.setResizable(false);
 		frame.setSize(800, 450);
 		frame.setVisible(true);
 	
 		//Background panel
-		bgPanel.setSize(850, 450);
-		bgPanel.setVisible(true);	
+		bgPanel.setSize(frame.getWidth(), frame.getHeight());
+		
+		//Name panel
+		namePanel.setLocation(10, 10);
+		namePanel.setSize(100, 100);
+		namePanel.setOpaque(false);
+		
+		//News Buttons
+		news0Button.setSize(200, 200);
+		news0Button.setLocation((frame.getWidth() / 2) - news0Button.getWidth(), (frame.getHeight() / 2) - news0Button.getHeight());
+		//news0Button.setLocation(100, 100);
+	
+		frame.add(namePanel);
+		frame.add(bgPanel);
+		frame.add(news0Button);
 	}
 	
 	public JFrame frame = new JFrame();
-	public JPanelWithBackground bgPanel = new JPanelWithBackground("http://127.0.0.1/Dofus/Launcher/Ressources/dofus259.jpeg");
+	public JPanelWithBackground bgPanel = new JPanelWithBackground("http://127.0.0.1/Dofus/Launcher/Ressources/dofus002.jpeg");
+	public JPanelWithBackground namePanel = new JPanelWithBackground("http://127.0.0.1/Dofus/Launcher/Ressources/dofus003.png");
+	public JButtonWithBackground news0Button = new JButtonWithBackground("http://127.0.0.1/Dofus/Launcher/Ressources/dofus004.jpeg");
+	
+	public void Test() {
+		
+	}
 }
--- src/proex/launcher/main/Extensions.java
+++ src/proex/launcher/main/Extensions.java
@@ -2,11 +2,11 @@ package proex.launcher.main;
 
 import java.awt.Graphics;
 import java.awt.Image;
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class Extensions {
@@ -15,10 +15,29 @@ public class Extensions {
 		  private static final long serialVersionUID = 1L;
 		private Image backgroundImage;
 
-		  // Some code to initialize the background image.
-		  // Here, we use the constructor to load the image. This
-		  // can vary depending on the use case of the panel.
 		  public JPanelWithBackground(String url) {
+		    try {
+				backgroundImage = ImageIO.read(new URL(url));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		  }
+
+		  public void paintComponent(Graphics g) {
+		    super.paintComponent(g);
+
+		    // Draw the background image.
+		    g.drawImage(backgroundImage, 0, 0, this);
+		  }
+		}
+	
+	public static class JButtonWithBackground extends JButton {
+
+		  private static final long serialVersionUID = 1L;
+		private Image backgroundImage;
+
+		  public JButtonWithBackground(String url) {
 		    try {
 				backgroundImage = ImageIO.read(new URL(url));
 			} catch (IOException e) {
